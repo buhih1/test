5ad40df817a641faee95daca486c0449ac00c831
busi <76762982+buhih1@users.noreply.github.com>
2022-11-09 20:30:02 +0900

    before jQuery

--- PNU/src/main/java/pnu/main/controller/MainController.java
+++ PNU/src/main/java/pnu/main/controller/MainController.java
@@ -1,14 +1,49 @@
 package pnu.main.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import pnu.user.service.UserService;
+import pnu.user.vo.UserVO;
 
 @Controller
 public class MainController {
-
+	
+	@Autowired
+	UserService userService;
+	
 	@RequestMapping(value="/main.do", method = RequestMethod.GET)
 	public String mainPage() {
-		return "main.html";
+		return "main.jsp";
+	}
+
+	@RequestMapping(value="/loginPage.do", method = RequestMethod.GET)
+	public String loginPage() {
+		return "login.jsp";
+	}
+
+	@RequestMapping(value="/login.do", method = RequestMethod.POST)
+	public String login(HttpSession session, @ModelAttribute UserVO user) {
+		if(userService.selectUsed(user.getUserId()).equals("Y") &&
+				userService.selectPwd(user.getUserId(), user.getPwd())) {
+			userService.setSesstion(session, user.getUserId());
+			
+			return "main.jsp";
+		} else {
+			return "login.jsp";
+		}
+	}
+	
+	@RequestMapping(value="/logout.do", method = RequestMethod.GET)
+	public String logout(HttpSession session) {
+		session.removeAttribute("USER");
+		return "login.jsp";
 	}
 }
+	
